f1b5d3b4896b6a115eacf504cbfdc7209026f6e1
김정혁 <jack1150@naver.com>
2024-01-27 19:35:33 +0900

    tailwind 적용

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -22,7 +22,7 @@ const MainPage = () => {
   };
 
   return (
-    <div>
+    <>
       메인 페이지
       {/* 로그인 페이지로 이동하는 링크 */}
       <br />
@@ -30,7 +30,7 @@ const MainPage = () => {
       <br />
       {/* 회원가입 페이지로 이동하는 링크 */}
       <Link to="/register">Register</Link>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/pages/productDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { db } from "@/firebase";
+
+const productDetail = () => {
+  return (
+    <>
+      <div className="all_product"></div>
+    </>
+  );
+};
+
+export default productDetail;
